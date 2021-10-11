55a6be21b152471e67d4190ca79a53ae62ad9b21
Mark003 <coderush@gmail.com>
2021-10-11 12:05:57 +0200

    Added numeric constant example to CapsQ. Revised the TargetPicker image.

--- More Lessons/CapsQ.cs
+++ More Lessons/CapsQ.cs
@@ -21,11 +21,11 @@ namespace CapsModifier
 
 		![](TargetPicker)
 
-		 Just use the up and down arrow keys, followed by the Enter key
-		 to select the location for the generated code.
+		 Just use the up/down arrow keys and the Enter key to select the 
+		 location for the generated code.
 		*/
 
-		string GetRelativePath(string str)
+		public static string GetRelativePath(string str)
 		{
 			int parentFolderIndex = str.IndexOf(@"\wwwroot\GameDev\Assets\");
 			if (parentFolderIndex >= 0)//`             Caps+Q ![](ArrowDown) to declare a constant.
@@ -33,11 +33,26 @@ namespace CapsModifier
 			return null;
 		}
 
+
+
+		public double Circumference(double radius)
+		{
+			return 6.283185307179586476925286766559 * radius;
+		}
+
+		public double AreaCircle(double radius)
+		{
+			//           ![](LookHere) Look here when you try the next one.
+			return 6.283185307179586476925286766559 * radius * radius / 2;
+		}
+
 		public double VolumeSphere(double radius)
 		{//`             Caps+Q ![](ArrowDown) to declare a constant (name it Tau).
 			return 2/3 * 6.283185307179586476925286766559 * radius * radius * radius;
 		}
 	}
+
+
 
 	//`++Great Work!
 
