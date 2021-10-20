b8cd4c158fea69cd8f045137754b031a85ab86ae
Mark003 <coderush@gmail.com>
2021-10-20 17:57:12 +0200

    Navigation Project

---
+++ Navigation/Markers.cs
@@ -0,0 +1,23 @@
+﻿namespace Navigation
+{
+	public class Markers
+	{
+    //`+Markers
+    /*
+      Some CodeRush features create code in *new locations* (and take 
+      you there so you can see the new code). 
+    
+      Before shifting the view, CodeRush drops a marker so you can 
+      return to where you were.
+
+      To get back, press Escape (or Alt+End).
+
+      ![](Gear;crcommand:Options:Editor\All Languages\Markers) << Marker settings.
+    
+
+       ![](Markers;https://www.youtube.com/watch?v=_awmO-WZ_vw&list=PL8h4jt35t1wgawacCN9wmxq1EN36CNUGk&index=23 ) << Learn more about Markers.
+    */
+
+    //` ![](GotIt;vscommand:File.Close)
+  }
+}
---
+++ Navigation/TargetPicker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Navigation
+{
+	class TargetPicker
+	{
+		//`+Target Picker
+		/*
+      Some features let you decide where code will be placed 
+      using a target picker. The target picker looks like this:
+
+      ![](TargetPicker;;;0.01782,0.01600)
+
+      The horizontal line specifies where the new code will go. 
+		
+			Move the line up or down with the Up/Down arrows and press 
+			Enter to accept the location.
+		
+			Escape cancels.
+    */
+
+		//` ![](GotIt;vscommand:File.Close)
+	}
+}
