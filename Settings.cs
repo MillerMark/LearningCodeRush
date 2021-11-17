7fc273a37b6d35e163a7b2490101ccd7e205f63c
Mark Miller <markm@devexpress.com>
2021-11-17 17:48:39 -0600

    Shortened intro text in _StartHere_.cs. Added a separate Settings.cs file with details on the settings overrides.

---
+++ Settings.cs
@@ -0,0 +1,26 @@
+﻿/*
+	This solution includes project-based CodeRush setting overrides to ensure 
+	a smoother learning experience.
+
+	These overrides may differ from your actual settings, which means features 
+	may work in this solution while still being disabled in other solutions.
+
+	The following settings have been overridden and may be different from 
+	your defaults:
+
+	![](Gear;crcommand:Options:Quick Setup) << Rich Comments, Markers, and Smart Cut/Copy are enabled.
+
+	Don't forget to run the Setup Wizard if you haven't yet. It's a fast 
+	way to enable or disable major CodeRush features (like Caps as a 
+	Modifier) for the way you work.
+
+			![](CodeRushSetup;crcommand:RunSetupWizard)  << Click to run the Setup Wizard */
+
+//`![](GotIt;vscommand:File.Close)
+
+
+//`![](DevExpress; www.devexpress.com )
+// Copyright © 2021 Developer Express Inc.
+//
+// All trademarks or registered trademarks are property of their 
+// respective owners.
